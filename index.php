9821451897dcc75cf8dbe788378e28fa04bb857e
IsraelM2345 <mollejonisrael3@gmail.com>
2025-03-07 10:02:54 +0800

    Change Files

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "your_database");
-$result = $conn->query("SELECT content FROM editor_content ORDER BY id DESC LIMIT 1");
-$row = $result->fetch_assoc();
-$savedContent = $row['content'] ?? ''; // Load last saved content
-?>
-
-<textarea name="textarea" id="default"><?php echo htmlspecialchars($savedContent); ?></textarea>
